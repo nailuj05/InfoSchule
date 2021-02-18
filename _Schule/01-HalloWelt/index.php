f2775140cfaab140e06bb67313d7c42ffe32d28c
nailuj05 <57530068+nailuj05@users.noreply.github.com>
2021-02-18 12:59:17 +0100

    Add files via upload

---
+++ _Schule/01-HalloWelt/index.php
@@ -0,0 +1,3 @@
+<?php
+    echo 'Hallo Welt!'; //Der string Hallo Welt wird über den echo Befehl als HTML zurück gegeben
+?>
---
+++ _Schule/02-Variablen/variablen.php
@@ -0,0 +1,9 @@
+<?php
+    $variable = 'Eine Zahl';                    //Eine Variable die einen string beinhaltet wird angelegt
+    echo $variable;                             //Echo gibt die Variable über HTML aus;
+    echo '<br>';                                //Neue Zeile
+    $variable2 = 123456;                        //Eine Variable die eine int beinhaltet wird angelegt
+    echo $variable2;                            //Die 2. Variable wird ausgegeben
+    echo '<br>';                                //Neue Zeile
+    echo $variable . ':' . $variable2 . '<br>'; //zur 1. Variable wird ein ':' und die 2. Variable hinzugefügt
+?>
