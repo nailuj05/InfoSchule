2b127dede51d1135e1e289cb2bcdf8506acf0be4
nailuj05 <julian.limburg@gmx.de>
2021-03-20 11:04:49 +0100

    While-Schleifen Aufgabe 3

---
+++ _Schule/03-Schleifen/whileCounter.php
@@ -0,0 +1,10 @@
+<?php
+
+$x = 100; //Eine Variable x wird erstellt und ihr wird der Wert 100 zugewiesen
+
+while($x > 0){      //Solange die Variable x größer als 0 ist wird der Code innerhalb der Schleife ausgeführt
+    echo $x.'<br>'; //Die Variable x wird ausgegeben
+    $x--;           //Der Wert der Variable x wird um eins verkleinert
+}
+
+?>
