e65b26b909dc58f7287a1186098869cfbe2fd2bc
nailuj05 <julian.limburg@gmx.de>
2021-07-01 21:46:21 +0200

    Moved functions to seperate file

---
+++ _Schule/08-Lotto/functions.php
@@ -0,0 +1,59 @@
+<?php
+    function IsNumInArray($array, $num){
+        if(count($array) == 0){ 
+            return false;
+        }
+
+        for($i = 0; $i < count($array); $i++){
+            if($array[$i] == $num){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    function RandArray(){
+        $lottozahlen = array();
+
+        while(count($lottozahlen) < 7){
+            $rand = rand(1, 49);
+
+            if(!IsNumInArray($lottozahlen, $rand)){
+                $lottozahlen[] = $rand;
+            }
+        }
+        return $lottozahlen;
+    }
+
+    function IsArraySorted($array){
+        $lastElement = $array[0];
+        $currentElement = null;
+
+        for($i = 1; $i < count($array); $i++){
+            $currentElement = $array[$i];
+
+            if($lastElement > $currentElement){
+                return false;
+            }
+            $lastElement = $currentElement;
+        }
+        return true;
+    }
+
+    function BubbleSort($array){
+        while(!IsArraySorted($array))
+        {
+            for ($i = 1; $i < count($array); $i++) { 
+                $curr = $array[$i];
+                $last = $array[$i - 1];
+
+                if($last > $curr){
+                    $array[$i] = $last;
+                    $array[$i - 1] = $curr;
+                }
+            }
+        }
+
+        return $array;
+    }
+?>
